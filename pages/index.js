f65bb98118736629d2a3cca0876c329c965643f2
Adonias Macedo de Aquino <120606346+AdoniasMacedo@users.noreply.github.com>
2026-07-05 18:30:10 +0000

    adiciona a pasta pages

---
+++ pages/index.js
@@ -0,0 +1,6 @@
+function Home()
+{
+    return <h1>A Sara Macaedo de Atito tem uma cabeça do tamanho de Júpiter. Eu acho que ela é uma alienígena. Se alguém ver essa mensagem, por favor, me salve.</h1>
+}
+
+export default Home;
