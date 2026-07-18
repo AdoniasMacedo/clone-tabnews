53b033da60ee287b1dd665733bfaab75650aed57
Adonias Macedo <120606346+AdoniasMacedo@users.noreply.github.com>
2026-07-18 03:22:50 +0000

    adiciona scripts `lint:check` e `lint:fix`

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # clone-tabnews
+
 Projeto criado para auxiliar nas aulas do https://curso.dev, onde iremos aprender a criar um projeto como o https://tabnews.com.br
--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,10 @@
-function Home()
-{
-    return <h1>A Sara Macaedo de Atito tem uma cabeça do tamanho de Júpiter. Eu acho que ela é uma alienígena. Se alguém ver essa mensagem, por favor, me salve. 👽</h1>
+function Home() {
+  return (
+    <h1>
+      A Sara Macaedo de Atito tem uma cabeça do tamanho de Júpiter. Eu acho que
+      ela é uma alienígena. Se alguém ver essa mensagem, por favor, me salve. 👽
+    </h1>
+  );
 }
 
 export default Home;
