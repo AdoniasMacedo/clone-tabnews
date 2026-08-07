49da096bbc4b3a67f18afde22e2b893b3bf5efc6
Adonias Macedo <120606346+AdoniasMacedo@users.noreply.github.com>
2026-08-07 14:48:05 +0000

    add a more robust ssl configuration method

--- infra/database.js
+++ infra/database.js
@@ -7,7 +7,7 @@ async function query(queryObject) {
     user: process.env.POSTGRES_USER,
     database: process.env.POSTGRES_DB,
     password: process.env.POSTGRES_PASSWORD,
-    ssl: process.env.NODE_ENV === "development" ? false : true,
+    ssl: querySSLValues(),
   });
 
   console.log("Credenciais do Postgres", {
@@ -16,6 +16,7 @@ async function query(queryObject) {
     user: process.env.POSTGRES_USER,
     database: process.env.POSTGRES_DB,
     password: process.env.POSTGRES_PASSWORD,
+    ssl: querySSLValues(),
   });
 
   try {
@@ -33,3 +34,13 @@ async function query(queryObject) {
 export default {
   query: query,
 };
+
+function querySSLValues() {
+  if (process.env.POSTGRES_CA) {
+    return {
+      ca: process.env.POSTGRES_CA,
+    };
+  }
+
+  return process.env.NODE_ENV === "development" ? false : true;
+}
