9abb75706802feaf0d4e833c8a14ae147c4cbd38
Adonias <adoniasmacedodeaquino@gmail.com>
2026-08-23 13:35:35 -0300

    feat: require `create:user` to access `/api/v1/users`

--- pages/api/v1/users/index.js
+++ pages/api/v1/users/index.js
@@ -5,7 +5,8 @@ import activation from "models/activations";
 
 const router = createRouter();
 
-router.post(postHandler);
+router.use(controller.injectAnonymousOrUser);
+router.post(controller.canRequest("create:user"), postHandler);
 
 export default router.handler(controller.errorHandlers);
 
